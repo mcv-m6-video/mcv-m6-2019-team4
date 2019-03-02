550ec4ed24d50c157e81bd0f47f960b868ae5b5c
Jon Poveda <jon.poveda@gmail.com>
2019-03-02 20:48:59 +0100

    feat: implement loading detection files

---
+++ utils/load_detections.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+from paths import AICITY_DIR
+from pathlib import Path
+
+
+def detections_to_bounding_boxes(detections: np.ndarray):
+    """ Convert detections to bounding boxes.
+
+    Transforms from
+    ```
+    [frame, left, top, width, height, score]
+    ```
+    to
+    ```
+    [frame, left, upper, right, lower, score]
+    ```
+     """
+    return np.stack(
+        (
+            detections[:, 0],
+            detections[:, 1],
+            detections[:, 2],
+            detections[:, 1] + detections[:, 3],
+            detections[:, 2] + detections[:, 4],
+            detections[:, 5],
+        ),
+        axis=1
+    )
+
+
+def load_detections(path: Path):
+    """ Load detections given a path.
+
+    Detections are formatted as
+
+    ```
+    [frame, left, top, width, height, score]
+    ```
+
+    where frame starts at 1.0 and all elements are float types
+    """
+    with path.open('r') as file:
+        data = [
+            line.strip().split(',')
+            for line in file.readlines()
+        ]
+
+    data = np.array(data, dtype=float)
+    # Delete unused columns (they are filled with '-1')
+    return np.delete(data, [1, 7, 8, 9], axis=1)
+
+
+def load_bounding_boxes(path: Path):
+    """ Load bounding boxes given a path.
+
+    Detections are formatted as
+
+    ```
+    [frame, left, upper, right, lower, score]
+    ```
+
+    where frame starts at 1.0 and all elements are float types
+    """
+    return detections_to_bounding_boxes(load_detections(path))
+
+
+if __name__ == '__main__':
+    filepath = AICITY_DIR.joinpath('det', 'det_mask_rcnn.txt')
+    detections = load_detections(filepath)
+    print(detections.shape)
+    bb = detections_to_bounding_boxes(detections)
+    print(bb.shape)
+
+    # print(detections[0,:])
+    # print(bb[0,:])
+
+    filepath = AICITY_DIR.joinpath('det', 'det_ssd512.txt')
+    detections = load_detections(filepath)
+    print(detections.shape)
+
+    filepath = AICITY_DIR.joinpath('det', 'det_yolo3.txt')
+    detections = load_detections(filepath)
+    print(detections.shape)
