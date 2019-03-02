4df664581999c7aed737e7fdf4c6982b03590661
marticobos <marticobos@hotmail.com>
2019-03-02 14:16:08 +0100

    modify ground truth extractor to class

--- utils/detectionExtractorGT.py
+++ utils/detectionExtractorGT.py
@@ -1,24 +1,38 @@
-def extractGT(file):
-    gt = []
-    with open(file) as fp:
-        for line in fp:
-            data = [float(elt.strip()) for elt in line.split(',')]
-            gt.append(data)
 
-    return gt
+class detectionExtractorGT():
+
+    def __init__(self, filePath):
+        self.file = filePath
+        self.gt = []
+        self.extractGT()
+
+    def extractGT(self):
+        self.gt = []
+        with open(self.file) as fp:
+            for line in fp:
+                data = [float(elt.strip()) for elt in line.split(',')]
+                self.gt.append(data)
+
+        return
+
+    def setFile (self, filePath):
+        self.file = filePath
 
 
-def getGTFrame (gtElement):
-    return gtElement[0]
+    def getGTFrame (self, i):
+        gtElement = self.gt[i]
+        return gtElement[0]
 
 
-def getGTID (gtElement):
-    return gtElement[1]
+    def getGTID (self,i ):
+        gtElement = self.gt[i]
+        return gtElement[1]
 
 
-def getGTBoundingBox (gtElement):
-    #BBformat [xA,yA, xB, yB]
-    BBox = [gtElement[2], gtElement[3], gtElement[2]+gtElement[4], gtElement[3]+gtElement[5]]
-    return BBox
+    def getGTBoundingBox (self, i):
+        #BBformat [xA,yA, xB, yB]
+        gtElement = self.gt[i]
+        BBox = [gtElement[2], gtElement[3], gtElement[2]+gtElement[4], gtElement[3]+gtElement[5]]
+        return BBox
 
 
