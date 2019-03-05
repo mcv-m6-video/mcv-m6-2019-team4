e8fb255cddab8db7928deeb261a82b946536bf3e
Ferran Pérez Gamonal <fperez.gamonal@gmail.com>
2019-03-05 23:21:32 +0100

    Added visualization, finishing touches

--- evaluation/flow_metrics.py
+++ evaluation/flow_metrics.py
@@ -70,11 +70,11 @@ def squared_difference_noc(tu, tv, u, v, mask, plot=0, method='MSEN'):
 
             gt_flow_img = flow_utils.flow_to_image(gt_flow)
             est_flow_img = flow_utils.flow_to_image(est_flow)
-            # error_img = flow_utils.flow_to_image(E)
+            error_img = E.transpose(1,0)
 
-            fig, ax = plt.subplots(3, 1, sharex='col', sharey='row')
-            ax[0, 0] = plt.imshow(gt_flow_img)
-            ax[1, 0] = plt.imshow(est_flow_img)
+            # fig, ax = plt.subplots(2, 1, sharex='col', sharey='row')
+            # ax[0, 0] = plt.imshow(gt_flow_img)
+            # ax[1, 0] = plt.imshow(est_flow_img)
             # ax[2, 0] = plt.
             plt.show()
             # TODO: test the above
