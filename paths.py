ebf597b505d90ccb015f2c10a210e78f5b4d27b7
Jon Poveda <jon.poveda@gmail.com>
2019-03-06 11:07:49 +0100

    [drop] add a reduced ground truth

--- paths.py
+++ paths.py
@@ -3,3 +3,4 @@ from pathlib import Path
 AICITY_DIR = Path('/home/jon/Datasets/MCV/video/AICity_data/train/S03/c010')
 # AICITY_ANNOTATIONS = AICITY_DIR.joinpath('Anotation_40secs_AICITY_S03_C010.xml')
 AICITY_ANNOTATIONS = Path(__file__).parent.joinpath('annotations.xml')
+AICITY_ANNOTATIONS = Path(__file__).parent.joinpath('some_annotations.xml')
