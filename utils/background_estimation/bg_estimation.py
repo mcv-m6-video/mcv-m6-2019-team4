46bc9ef32bfcf34ccf1d2c0170eff488df952777
Ferran Pérez Gamonal <fperez.gamonal@gmail.com>
2019-03-10 16:59:38 +0100

    feat: added functions as class methods

--- utils/background_estimation/bg_estimation.py
+++ utils/background_estimation/bg_estimation.py
@@ -5,6 +5,7 @@ import cv2
 import glob
 import os
 import matplotlib.pyplot as plt
+from multiprocessing import Pool, Value
 
 
 SIGMA_THR = 2.5  # number of standard deviations to define the background/foreground threshold
@@ -38,11 +39,14 @@ class BackgroundModel(object):
 
     def __init__(self, height, width, channels, threshold=SIGMA_THR, rho=RHO):
         if channels == 1:
+            self.sum = np.zeros((height, width)).astype(np.uint64)
             self.mean = np.zeros((height, width)).astype(np.uint64)
-            self.variance = np.zeros((height, width)).astype(np.float64)
+            self.std = np.zeros((height, width)).astype(np.float64)
+
         elif channels == 3:
-            self.mean = np.zeros((height, width, channels))
-            self.variance = np.zeros((height, width, channels))
+            self.sum = np.zeros((height, width, channels)).astype(np.uint64)
+            self.mean = np.zeros((height, width, channels)).astype(np.uint64)
+            self.std = np.zeros((height, width, channels)).astype(np.float64)
         else:
             print("FATAL: wrong number of channels, the frame must be either in grayscale or RGB")
             return
@@ -73,88 +77,89 @@ class BackgroundModel(object):
         var_new = (1 - self.rho) * var_old + self.rho * np.square(curr_frame - new_mean)
         return var_new
 
+    def update_cumulative_frame(self, image):
+        # Add current's frame value to accumulator
+        self.sum += image
 
-# TODO: move estimation functions into the class
+    def estimate_bg_single_gaussian(self, image_file_names, roi_filename):
+        """
+        returns an image with channels:
+        #   1: mean image
+        #   2: stdev image
+        #   3: roi image
 
-def update_cumulative_frame(image, sum_frame):
-    # Add current's frame value to a
-    sum_frame += image
-    return sum_frame
+        :param image_file_names: filenames of the background images
+        :param roi_filename: ROI mask
+        :return: tuple composed of the mean, stddev and ROI images
+
+        Note: to significantly reduce the memory footprint, we need to loop twice through the frame list:
+        Once to compute the mean frame, and the second to compute the stddev image
+        """
+        # Not used right now
+        roi_image = cv2.imread(roi_filename, 0)  # Take advantage that the ROI has the same dim. as the sequence frames
+
+        # height, width = roi_image.shape
+        # To avoid overflow, make the accumulator a uint64
+        # sum_image = np.zeros((height, width)).astype(np.uint64)
+        # std_image = np.zeros((height, width)).astype(np.float64)
+
+        # 1. Estimate mean image
+        for frame_name in image_file_names:
+            # Read frame
+            curr_frame = cv2.imread(frame_name, 0)
+            # Accumulate image
+            self.update_cumulative_frame(curr_frame)
+
+        self.mean = self.sum / len(image_file_names)
+
+        # 2. Estimate stddev image
+        for frame_name in image_file_names:
+            # Read frame
+            curr_frame = cv2.imread(frame_name, 0)
+            self.std += np.square(curr_frame - self.mean)
+
+        # 3 dim array with all the frames ==> THIS USES A LOOOT OF MEMORY
+        # list_images = [cv2.imread(fp,0) for fp in image_file_names]
+        # images = np.dstack(list_images)
+        #
+        # # mean and stdev of each pixel along all frames
+        # mean = np.mean(images, axis=2)
+        # stdev = np.std(images, axis=2)
+
+        # gaussian_image = np.dstack((mean_image, std_image, roi_image))
+
+        # return gaussian_image
+
+    def bg_segmentation_single_gaussian(self, image_file_name):
+        """
+        Segments the current image into two classes: background ('0's) and foreground ('1's).
+        :param image_file_name: path to the frame to segment.
+        :param bg_estimation: background estimation model.
+        :return: an image with black on the background, white on the foreground.
+        """
+        image = cv2.imread(image_file_name, 0)
+
+        # mean = # bg_estimation[:, :, 0]
+        # std = bg_estimation[:, :, 1] * threshold
+        dist = np.abs(self.mean - image)
+        diff = self.std * self.threshold - dist
+        diff[diff < 0] = 255
+        diff[diff != 255] = 0
+
+        return diff.astype(np.uint8)
 
 
-def estimate_bg_single_gaussian(image_file_names, roi_filename):
-    """
-    returns an image with channels:
-    #   1: mean image
-    #   2: stdev image
-    #   3: roi image
-
-    :param image_file_names: filenames of the background images
-    :param roi_filename: ROI mask
-    :return: tuple composed of the mean, stddev and ROI images
-
-    Note: to significantly reduce the memory footprint, we need to loop twice through the frame list:
-    Once to compute the mean frame, and the second to compute the stddev image
-    """
-    roi_image = cv2.imread(roi_filename, 0)  # Take advantage that the ROI has the same dim. as the sequence frames
-
-    height, width = roi_image.shape
-    # To avoid overflow, make the accumulator a uint64
-    sum_image = np.zeros((height, width)).astype(np.uint64)
-    std_image = np.zeros((height, width)).astype(np.float64)
-
-    # 1. Estimate mean image
-    for frame in image_file_names:
-        # Read frame
-        curr_frame = cv2.imread(frame, 0)
-        # Accumulate image
-        sum_image = update_cumulative_frame(curr_frame, sum_image)
-
-    mean_image = sum_image / len(image_file_names)
-
-    # 2. Estimate stddev image
-    for frame in image_file_names:
-        # Read frame
-        curr_frame = cv2.imread(frame, 0)
-        std_image += np.square(curr_frame - mean_image)
-
-    # 3 dim array with all the frames
-    # list_images = [cv2.imread(fp,0) for fp in image_file_names]
-    # images = np.dstack(list_images)
-    #
-    # # mean and stdev of each pixel along all frames
-    # mean = np.mean(images, axis=2)
-    # stdev = np.std(images, axis=2)
-
-    gaussian_image = np.dstack((mean_image, std_image, roi_image))
-
-    return gaussian_image
-
-
-def bg_segmentation_single_gaussian(image_file_name, bg_estimation, threshold=SIGMA_THR):
-    """
-    Segments the current image into two classes: background ('0's) and foreground ('1's).
-    :param image_file_name: path to the frame to segment.
-    :param bg_estimation: background estimation model.
-    :param threshold: threshold that determines what is background/foreground.
-    :return: an image with black on the background, white on the foreground.
-    """
-    image = cv2.imread(image_file_name, 0)
-
-    mean = bg_estimation[:, :, 0]
-    std = bg_estimation[:, :, 1] * threshold
-    dist = np.abs(mean - image)
-    diff = std - dist
-    diff[diff < 0] = 255
-    diff[diff != 255] = 0
-
-    return diff.astype(np.uint8)
+def rapid_variance(samples):
+    s0 = sum(1 for x in samples)
+    s1 = sum(x for x in samples)
+    s2 = sum(x * x for x in samples)
+    return s0, s1, s2
 
 
 if __name__ == '__main__':  # Consider removing this main() as 'Task 1' is almost identical (+ output to video)
     viz = True
     # Define path to video frames
-    filepaths = sorted(glob.glob(os.path.join(AICITY_DIR, 'vdo_frames/*.png')))  # change folder name (?)
+    filepaths = sorted(glob.glob(os.path.join(AICITY_DIR, 'vdo_frames/image-????.png')))  # change folder name (?)
     roi_path = os.path.join(AICITY_DIR, 'roi.jpg')
     percent_back = 25
     num_frames = len(filepaths)
@@ -162,19 +167,31 @@ if __name__ == '__main__':  # Consider removing this main() as 'Task 1' is almos
     # Get back frames' names
     back_list = filepaths[:num_backFrames]
 
-    # Define background model
-    bg_est = estimate_bg_single_gaussian(back_list, roi_path)
+    first_frame = cv2.imread(back_list[0], 0)
+    height, width = first_frame.shape
+    channels = 1
 
-    # Detect foregound (rest of the sequence)
-    fore_list = filepaths[num_backFrames+1:]
+    # Define background model
+    bg_model = BackgroundModel(height, width, channels, SIGMA_THR, RHO)
+    bg_model.estimate_bg_single_gaussian(back_list, roi_path)  # MUST speed this up, it takes more than a minute
+
     if viz:
         plt.figure()
+        plt.imshow(bg_model.mean, cmap='gray')
+        plt.show()
+
+    # Detect foregound (rest of the sequence)
+    fore_list = filepaths[num_backFrames:]
+
+    # Define video writer
+    video_name = 'background_estimation_single_gaussian_f.avi'
+    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), 10, (width, height))
 
     for frame in fore_list:
         print("Estimating frame: '{0}'".format(frame))
-        segm = bg_segmentation_single_gaussian(frame, bg_est)
-        if viz:
-            image = cv2.cvtColor(segm, cv2.COLOR_GRAY2BGR)
+        segm = bg_model.bg_segmentation_single_gaussian(frame)
+        image = cv2.cvtColor(segm, cv2.COLOR_GRAY2BGR)
+        video.write(image)
 
 
 
