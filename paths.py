8f1942a90a8f6b5784b5ae62308e6a6801e1a9cc
Jon Poveda <jon.poveda@gmail.com>
2019-03-13 11:12:24 +0100

    fix: increase multiplatform compatibility

--- paths.py
+++ paths.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
-AICITY_DIR = Path('../data/AICity_data/train/S03/c010')
+PROJECT_ROOT = Path(__file__).parent
+AICITY_DIR = PROJECT_ROOT.joinpath('data', 'AICity_data', 'train', 'S03', 'c010')
 # AICITY_ANNOTATIONS = AICITY_DIR.joinpath('Anotation_40secs_AICITY_S03_C010.xml')
-AICITY_ANNOTATIONS = Path(__file__).parent.joinpath('annotations.xml')
-AICITY_ANNOTATIONS = Path(__file__).parent.joinpath('some_annotations.xml')
+AICITY_ANNOTATIONS = PROJECT_ROOT.joinpath('annotations.xml')
+AICITY_ANNOTATIONS = PROJECT_ROOT.joinpath('some_annotations.xml')
 
-DATA_DIR = Path(__file__).parent.joinpath('data')
+DATA_DIR = PROJECT_ROOT.joinpath('data')
