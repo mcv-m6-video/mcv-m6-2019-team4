882dfcf01f05d5aa684245f476a26cc3a6c52b4f
Jon Poveda <jon.poveda@gmail.com>
2019-03-20 10:29:48 +0100

    chore: add full annotations of AICITY

--- paths.py
+++ paths.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent
-AICITY_DIR = PROJECT_ROOT.joinpath('..', 'data', 'AICity_data', 'train', 'S03', 'c010')
-# AICITY_ANNOTATIONS = AICITY_DIR.joinpath('Anotation_40secs_AICITY_S03_C010.xml')
-AICITY_ANNOTATIONS = PROJECT_ROOT.joinpath('annotations.xml')
-AICITY_ANNOTATIONS = PROJECT_ROOT.joinpath('some_annotations.xml')
+AICITY_DIR = PROJECT_ROOT.joinpath('data', 'AICity_data', 'train', 'S03',
+                                   'c010')
+AICITY_ANNOTATIONS = PROJECT_ROOT.joinpath('full_annotations.xml')
 
 DATA_DIR = PROJECT_ROOT.joinpath('data')
